d747b6e55c906f96136d3b0f808f90d4f13aa1ff
PepeSchwerter <jose.schwerterd@sansano.usm.cl>
2025-04-18 16:31:29 -0400

    second commit

--- Characters/player_cat.gd
+++ Characters/player_cat.gd
@@ -3,31 +3,41 @@ extends CharacterBody2D
 @export var move_speed : float = 100
 @export var accel : float = 8
 @export var starting_direction : Vector2 = Vector2(0, 1)
+@export var current_tool: DataTypes.Tools = DataTypes.Tools.None
+@export var hit_component_collision_shape: CollisionShape2D
 
 @onready var animation_tree : AnimationTree = $AnimationTree
-@onready var state_machine : AnimationNodeStateMachinePlayback = animation_tree.get("parameters/playback")
+
+var is_chopping : bool = false
+
 func _ready():
 	update_animation_parameters(starting_direction)
+	hit_component_collision_shape.disabled = true
+	hit_component_collision_shape.position = Vector2.ZERO
 
 func _physics_process(_delta):
 	if Input.is_action_pressed("ui_cancel"):
 		get_tree().quit()
 		
-	var input_direction = Input.get_vector("left", "right", "up", "down")
+	var input_direction = Input.get_vector("walk_left", "walk_right", "walk_up", "walk_down")
 
 	update_animation_parameters(input_direction)
 	velocity.x = move_toward(velocity.x, input_direction.x * move_speed, accel)
-	velocity.y = move_toward(velocity.y, input_direction.y * move_speed, accel)
-	pick_new_state()
+	velocity.y = move_toward(velocity.y, input_direction .y * move_speed, accel)
 	move_and_slide()
 
 func update_animation_parameters(move_input : Vector2):
 	if(move_input != Vector2.ZERO):
 		animation_tree.set("parameters/Idle/blend_position", move_input)
 		animation_tree.set("parameters/Walk/blend_position", move_input)
+		animation_tree.set("parameters/Chop/blend_position", move_input)
+		animation_tree.
 
-func pick_new_state():
-	if(velocity != Vector2.ZERO):
-		state_machine.travel("Walk")
-	else:
-		state_machine.travel("Idle")
+func check_for_actions():
+	if Input.is_action_just_pressed("action"):
+		is_chopping = true 
+	if animation_tree
+
+
+func _on_animation_tree_animation_finished(anim_name: StringName) -> void:
+	pass # Replace with function body.
